ac6466296bad377cdaf98414e73aa6eb406f9a6a
Sébastien Doeraene <sjrdoeraene@gmail.com>
2016-07-21 12:12:22 +0200

    Implement the SHA-512 benchmark.

--- src/main/java/be/doeraene/teavmlongbench/Client.java
+++ src/main/java/be/doeraene/teavmlongbench/Client.java
@@ -1,13 +1,198 @@
 package be.doeraene.teavmlongbench;
 
+import org.teavm.jso.dom.events.Event;
+import org.teavm.jso.dom.events.EventListener;
 import org.teavm.jso.dom.html.HTMLDocument;
 import org.teavm.jso.dom.html.HTMLElement;
+import org.teavm.jso.dom.html.HTMLButtonElement;
 
 public class Client {
-    public static void main(String[] args) {
-        HTMLDocument document = HTMLDocument.current();
-        HTMLElement div = document.createElement("div");
-        div.appendChild(document.createTextNode("TeaVM generated element"));
-        document.getBody().appendChild(div);
+  public static void main(String[] args) {
+    HTMLDocument document = HTMLDocument.current();
+    HTMLElement div = document.createElement("div");
+    document.getBody().appendChild(div);
+    HTMLButtonElement button =
+        (HTMLButtonElement) document.createElement("button");
+    div.appendChild(button);
+    HTMLElement resultLabel = document.createElement("p");
+    div.appendChild(resultLabel);
+
+    button.appendChild(document.createTextNode("Run bench"));
+    button.addEventListener("click", new EventListener<Event>() {
+      public void handleEvent(Event evt) {
+        resultLabel.clear();
+        resultLabel.appendChild(document.createTextNode("Running ..."));
+
+        String benchmarkResult = report();
+
+        resultLabel.clear();
+        resultLabel.appendChild(document.createTextNode(benchmarkResult));
+      }
+    });
+    document.getBody().appendChild(div);
+  }
+
+  /** Run the benchmark the specified number of milliseconds and return
+   *  the average execution time in microseconds.
+   */
+  private static double runBenchmark(long timeMinimum, int runsMinimum) {
+    int runs = 0;
+    long startTime = System.currentTimeMillis();
+    long stopTime = startTime + timeMinimum;
+    long currentTime = startTime;
+
+    do {
+      run();
+      runs++;
+      currentTime = System.currentTimeMillis();
+    } while (currentTime < stopTime || runs < runsMinimum);
+
+    long elapsed = currentTime - startTime;
+    return 1000.0 * elapsed / runs;
+  }
+
+  private static void warmUp() {
+    runBenchmark(100, 2);
+  }
+
+  private static String report() {
+    warmUp();
+    double avg = runBenchmark(2000, 5);
+    return "Average: " + avg + "us";
+  }
+
+  private static void run() {
+    if (!selfTest(false))
+      throw new AssertionError("Self test failed");
+  }
+
+  private static final String as() {
+    StringBuilder as = new StringBuilder(1000);
+    for (int i = 0; i < 1000; i++)
+      as.append('a');
+    return as.toString();
+  }
+
+  private static final byte[] asBytes(int[] values) {
+    byte[] bytes = new byte[values.length];
+    for (int i = 0; i < values.length; i++)
+      bytes[i] = (byte) values[i];
+    return bytes;
+  }
+
+  /*
+   * FIPS-180-2 test vectors
+   */
+  private static final byte[][] sha512TestBuf = new byte[][] {
+    "abc".getBytes(),
+    ("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmn" +
+      "hijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu").getBytes(),
+    as().getBytes()
+  };
+
+  private static final byte[][] sha512TestSum = new byte[][] {
+    /*
+     * SHA-384 test vectors
+     */
+    asBytes(new int[] {
+      0xCB, 0x00, 0x75, 0x3F, 0x45, 0xA3, 0x5E, 0x8B,
+      0xB5, 0xA0, 0x3D, 0x69, 0x9A, 0xC6, 0x50, 0x07,
+      0x27, 0x2C, 0x32, 0xAB, 0x0E, 0xDE, 0xD1, 0x63,
+      0x1A, 0x8B, 0x60, 0x5A, 0x43, 0xFF, 0x5B, 0xED,
+      0x80, 0x86, 0x07, 0x2B, 0xA1, 0xE7, 0xCC, 0x23,
+      0x58, 0xBA, 0xEC, 0xA1, 0x34, 0xC8, 0x25, 0xA7
+    }),
+    asBytes(new int[] {
+      0x09, 0x33, 0x0C, 0x33, 0xF7, 0x11, 0x47, 0xE8,
+      0x3D, 0x19, 0x2F, 0xC7, 0x82, 0xCD, 0x1B, 0x47,
+      0x53, 0x11, 0x1B, 0x17, 0x3B, 0x3B, 0x05, 0xD2,
+      0x2F, 0xA0, 0x80, 0x86, 0xE3, 0xB0, 0xF7, 0x12,
+      0xFC, 0xC7, 0xC7, 0x1A, 0x55, 0x7E, 0x2D, 0xB9,
+      0x66, 0xC3, 0xE9, 0xFA, 0x91, 0x74, 0x60, 0x39
+    }),
+    asBytes(new int[] {
+      0x9D, 0x0E, 0x18, 0x09, 0x71, 0x64, 0x74, 0xCB,
+      0x08, 0x6E, 0x83, 0x4E, 0x31, 0x0A, 0x4A, 0x1C,
+      0xED, 0x14, 0x9E, 0x9C, 0x00, 0xF2, 0x48, 0x52,
+      0x79, 0x72, 0xCE, 0xC5, 0x70, 0x4C, 0x2A, 0x5B,
+      0x07, 0xB8, 0xB3, 0xDC, 0x38, 0xEC, 0xC4, 0xEB,
+      0xAE, 0x97, 0xDD, 0xD8, 0x7F, 0x3D, 0x89, 0x85
+    }),
+
+    /*
+     * SHA-512 test vectors
+     */
+    asBytes(new int[] {
+      0xDD, 0xAF, 0x35, 0xA1, 0x93, 0x61, 0x7A, 0xBA,
+      0xCC, 0x41, 0x73, 0x49, 0xAE, 0x20, 0x41, 0x31,
+      0x12, 0xE6, 0xFA, 0x4E, 0x89, 0xA9, 0x7E, 0xA2,
+      0x0A, 0x9E, 0xEE, 0xE6, 0x4B, 0x55, 0xD3, 0x9A,
+      0x21, 0x92, 0x99, 0x2A, 0x27, 0x4F, 0xC1, 0xA8,
+      0x36, 0xBA, 0x3C, 0x23, 0xA3, 0xFE, 0xEB, 0xBD,
+      0x45, 0x4D, 0x44, 0x23, 0x64, 0x3C, 0xE8, 0x0E,
+      0x2A, 0x9A, 0xC9, 0x4F, 0xA5, 0x4C, 0xA4, 0x9F
+    }),
+    asBytes(new int[] {
+      0x8E, 0x95, 0x9B, 0x75, 0xDA, 0xE3, 0x13, 0xDA,
+      0x8C, 0xF4, 0xF7, 0x28, 0x14, 0xFC, 0x14, 0x3F,
+      0x8F, 0x77, 0x79, 0xC6, 0xEB, 0x9F, 0x7F, 0xA1,
+      0x72, 0x99, 0xAE, 0xAD, 0xB6, 0x88, 0x90, 0x18,
+      0x50, 0x1D, 0x28, 0x9E, 0x49, 0x00, 0xF7, 0xE4,
+      0x33, 0x1B, 0x99, 0xDE, 0xC4, 0xB5, 0x43, 0x3A,
+      0xC7, 0xD3, 0x29, 0xEE, 0xB6, 0xDD, 0x26, 0x54,
+      0x5E, 0x96, 0xE5, 0x5B, 0x87, 0x4B, 0xE9, 0x09
+    }),
+    asBytes(new int[] {
+      0xE7, 0x18, 0x48, 0x3D, 0x0C, 0xE7, 0x69, 0x64,
+      0x4E, 0x2E, 0x42, 0xC7, 0xBC, 0x15, 0xB4, 0x63,
+      0x8E, 0x1F, 0x98, 0xB1, 0x3B, 0x20, 0x44, 0x28,
+      0x56, 0x32, 0xA8, 0x03, 0xAF, 0xA9, 0x73, 0xEB,
+      0xDE, 0x0F, 0xF2, 0x44, 0x87, 0x7E, 0xA6, 0x0A,
+      0x4C, 0xB0, 0x43, 0x2C, 0xE5, 0x77, 0xC3, 0x1B,
+      0xEB, 0x00, 0x9C, 0x5C, 0x2C, 0x49, 0xAA, 0x2E,
+      0x4E, 0xAD, 0xB2, 0x17, 0xAD, 0x8C, 0xC0, 0x9B
+    })
+  };
+
+  /*
+   * Checkup routine
+   */
+  private static boolean selfTest(boolean verbose) {
+    for (int i = 0; i < sha512TestSum.length; i++) {
+      int j = i % 3;
+      boolean is384 = i < 3;
+
+      if (verbose) {
+        System.out.print("  SHA-" + (is384 ? 384 : 512) + " test " +
+            (j + 1) + ": ");
+      }
+
+      SHA512Context ctx = new SHA512Context(is384);
+      byte[] buf = sha512TestBuf[j];
+
+      if (j == 2) {
+        for (int x = 0; x < 1000; x++)
+          ctx.update(buf, buf.length);
+      } else {
+        ctx.update(buf, buf.length);
+      }
+
+      byte[] sha512sum = new byte[is384 ? 48 : 64];
+      ctx.finish(sha512sum);
+
+      if (!java.util.Arrays.equals(sha512sum,  sha512TestSum[i])) {
+        if (verbose)
+          System.out.println("failed");
+        return false;
+      }
+
+      if (verbose)
+        System.out.println("passed");
     }
+
+    if (verbose)
+      System.out.println();
+
+    return true;
+  }
 }
